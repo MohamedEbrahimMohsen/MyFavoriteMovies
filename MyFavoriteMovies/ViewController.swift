533befd3e9cab190e65100141dc1b5371f42712a
Mohamed Mohsen <Mohamed.Ebrahim.Mohsen@gmail.comgit config --global user.name MohamedEbrahimMohsen>
2019-05-06 21:41:58 +0200

    Initial Commit

---
+++ MyFavoriteMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFavoriteMovies
+//
+//  Created by Mohamed Mohsen on 5/6/19.
+//  Copyright © 2019 Mohamed Mohsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
